72eec51526afd165b4e58490fb86e2c7c0ab6af9
Madhavi Mullapudi <madhavi.0108@gmail.com>
2020-03-27 18:41:33 -0700

    Revert "Migrating HTMLs to handlebars"

--- server.js
+++ server.js
@@ -21,12 +21,6 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-// Set Handlebars
-var exphbs = require("express-handlebars");
-
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
-
 // Requiring our routes
 require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
