432217af040470cf7b412548638123e452af10c9
iskona <isony.d@gmail.com>
2020-03-29 17:59:16 -0700

    fixed a couple of style errors showing in browser

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,19 +1,18 @@
-// Requiring path to so we can use relative routes to our HTML files
-var path = require("path");
-
 // Requiring our custom middleware for checking if a user is logged in
 var isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function(app) {
   app.get("/", function(req, res) {
     res.render("index", {
-      title: "DevHire"
+      title: "DevHire",
+      style: "style.css"
     });
   });
 
   app.get("/signup", function(req, res) {
     res.render("signup", {
       title: "DevHire Sign Up",
+      style: "style.css",
       js: "signup.js"
     });
   });
@@ -21,6 +20,7 @@ module.exports = function(app) {
   app.get("/login", function(req, res) {
     res.render("login", {
       title: "DevHire Log In",
+      style: "style.css",
       js: "login.js"
     });
   });
@@ -33,7 +33,7 @@ module.exports = function(app) {
       title: "DevHire Client",
       style: "clientForm.css",
       js: "client.js"
-    })
+    });
   });
 
   app.get("/clientProfile", isAuthenticated, function(req, res) {
@@ -41,7 +41,7 @@ module.exports = function(app) {
       title: "DevHire Client Profile",
       style: "clientProfile.css",
       js: "clientProfile.js"
-    })
+    });
   });
 
   app.get("/developer", isAuthenticated, function(req, res) {
@@ -49,7 +49,7 @@ module.exports = function(app) {
       title: "DevHire Developer",
       style: "developer.css",
       js: "developer.js"
-    })
+    });
   });
 
   app.get("/developerProfile", isAuthenticated, function(req, res) {
@@ -57,21 +57,23 @@ module.exports = function(app) {
       title: "DevHire Developer Profile",
       style: "developerProfile.css",
       js: "developerProjects.js"
-    })
+    });
   });
 
   //for latest view profile page
   app.get("/viewProfile", isAuthenticated, function(req, res) {
     res.render("viewProfile", {
       title: "DevHire View Developer Profile",
+      style: "style.css",
       js: "viewProfile.js"
-    })
+    });
   });
 
   app.get("/view-update-developer", isAuthenticated, function(req, res) {
     res.render("viewUpdateDeveloper", {
       title: "DevHire Update Developer Profile",
+      style: "style.css",
       js: "developerProfile.js"
-    })
+    });
   });
 };
